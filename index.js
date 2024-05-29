04a3494fcec314fc30be52c6c853636efff3c804
ShivanshuPanwar <shivanshupanwar96@gmail.com>
2024-05-29 17:43:19 +0530

    remove cors

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-const cors = require("cors");
-
 const fs = require("fs");
 
 const https = require("https");
@@ -22,39 +20,70 @@ require("./config/db");
 
 const port = process.env.PORT || 6001;
 
-// Enable CORS
-app.use(cors());
+// Middleware to check the origin of incoming requests
+const allowOnlyFromFrontend = (req, res, next) => {
+  const allowedOrigin = "https://systaldyn.in"; // Replace with your frontend URL   // https://yourfrontendurl.com
+  const origin = req.headers.origin;
+  if (origin === allowedOrigin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  } else {
+    res.status(403).json({ error: "Unauthorized" });
+  }
+};
 
 app.use(express.json());
 
+// Apply the middleware to all routes
+app.use(allowOnlyFromFrontend);
+
 app.use("/api/user", userRoute);
-
 app.use("/api/leave", leaveRoute);
-
 app.use("/api/asset", assetRoute);
-
 app.use("/api/contactUs", contactUsRoute);
 
 app.listen(port, () => {
   console.log("Server running on Port 6001");
 });
 
-// const options = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/your_domain.com/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/your_domain.com/fullchain.pem"),
-// };
+// const express = require("express");
 
-// https.createServer(options, app).listen(6001, () => {
-//   console.log("Server is running on https://your_domain.com:6001");
+// const cors = require("cors");
+
+// const fs = require("fs");
+
+// const https = require("https");
+
+// require("dotenv").config();
+
+// const userRoute = require("./routes/userRoute");
+
+// const leaveRoute = require("./routes/leaveRoute");
+
+// const assetRoute = require("./routes/assetRoute");
+
+// const contactUsRoute = require("./routes/contactUsRoute");
+
+// const app = express();
+
+// require("./config/db");
+
+// const port = process.env.PORT || 6001;
+
+// // Enable CORS
+// app.use(cors());
+
+// app.use(express.json());
+
+// app.use("/api/user", userRoute);
+
+// app.use("/api/leave", leaveRoute);
+
+// app.use("/api/asset", assetRoute);
+
+// app.use("/api/contactUs", contactUsRoute);
+
+// app.listen(port, () => {
+//   console.log("Server running on Port 6001");
 // });
-
-// Optional: Redirect HTTP to HTTPS
-// const http = require("http");
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-//     res.end();
-//   })
-//   .listen(80, () => {
-//     console.log("HTTP Server running on port 80 and redirecting to HTTPS");
-//   });
