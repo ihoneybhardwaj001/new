ccc15cfddf5b3a82d7a81609cdd36659af950135
ShivanshuPanwar <shivanshupanwar96@gmail.com>
2024-05-29 17:52:03 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const express = require("express");
-
 const fs = require("fs");
-
 const https = require("https");
-
 require("dotenv").config();
-
 const userRoute = require("./routes/userRoute");
-
 const leaveRoute = require("./routes/leaveRoute");
-
 const assetRoute = require("./routes/assetRoute");
-
 const contactUsRoute = require("./routes/contactUsRoute");
 
 const app = express();
-
 require("./config/db");
 
 const port = process.env.PORT || 6001;
 
 // Middleware to check the origin of incoming requests
 const allowOnlyFromFrontend = (req, res, next) => {
-  const allowedOrigin = "https://systaldyn.in"; // Replace with your frontend URL   // https://yourfrontendurl.com
+  const allowedOrigin = "http://systaldyn.in"; // Replace with your frontend URL
   const origin = req.headers.origin;
   if (origin === allowedOrigin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    ); // Add OPTIONS method
     next();
   } else {
     res.status(403).json({ error: "Unauthorized" });
@@ -37,6 +33,11 @@ app.use(express.json());
 
 // Apply the middleware to all routes
 app.use(allowOnlyFromFrontend);
+
+// Handle OPTIONS requests separately
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/leave", leaveRoute);
