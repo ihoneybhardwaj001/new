31d4cfe3584e42a89631130e2302b230c6321a38
ShivanshuPanwar <shivanshupanwar96@gmail.com>
2024-05-30 11:26:38 +0530

    openssl update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const https = require("https");
+const cors = require("cors");
 require("dotenv").config();
 const userRoute = require("./routes/userRoute");
 const leaveRoute = require("./routes/leaveRoute");
@@ -10,42 +11,57 @@ const contactUsRoute = require("./routes/contactUsRoute");
 const app = express();
 require("./config/db");
 
-const port = process.env.PORT || 6001;
+// const port = process.env.PORT || 6001;
+const port = process.env.PORT || 443;
+
+// SSL/TLS certificates
+const options = {
+  key: fs.readFileSync("C:/Users/asus/key.pem"),
+  cert: fs.readFileSync("C:/Users/asus/cert.pem"),
+};
 
 // Middleware to check the origin of incoming requests
-const allowOnlyFromFrontend = (req, res, next) => {
-  const allowedOrigin = "http://systaldyn.in"; // Replace with your frontend URL
-  const origin = req.headers.origin;
-  if (origin === allowedOrigin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    ); // Add OPTIONS method
-    next();
-  } else {
-    res.status(403).json({ error: "Unauthorized" });
-  }
-};
+// const allowOnlyFromFrontend = (req, res, next) => {
+//   const allowedOrigin = "http://systaldyn.in"; // Replace with your frontend URL
+//   const origin = req.headers.origin;
+//   if (origin === allowedOrigin) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, DELETE, OPTIONS"
+//     ); // Add OPTIONS method
+//     next();
+//   } else {
+//     res.status(403).json({ error: "Unauthorized" });
+//   }
+// };
 
 app.use(express.json());
 
 // Apply the middleware to all routes
-app.use(allowOnlyFromFrontend);
+// app.use(allowOnlyFromFrontend);
 
 // Handle OPTIONS requests separately
-app.options("*", (req, res) => {
-  res.sendStatus(200);
-});
+// app.options("*", (req, res) => {
+//   res.sendStatus(200);
+// });
+
+// Enable CORS
+app.use(cors());
 
 app.use("/api/user", userRoute);
 app.use("/api/leave", leaveRoute);
 app.use("/api/asset", assetRoute);
 app.use("/api/contactUs", contactUsRoute);
 
-app.listen(port, () => {
-  console.log("Server running on Port 6001");
+// app.listen(port, () => {
+//   console.log("Server running on Port 6001");
+// });
+
+// HTTPS server
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server running on https://localhost:${port}`);
 });
 
 // const express = require("express");
